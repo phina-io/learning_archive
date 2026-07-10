fdb800334fb357b031a7ca13b5956b514b9b2a13
eun-0 <ey8973@gmail.com>
2026-07-10 16:32:20 +0900

    feat: 4일차 JavaScript 수업 실습 코드 학습

---
+++ OZ_AI_5/JS/Day04/practice/todo.js
@@ -0,0 +1,78 @@
+// 할 일을 입력하고, 추가 버튼을 누르면,
+// 할 일 목록에 할 일이 추가되어야 함
+
+const todoInput = document.querySelector("#todo-input")
+const todoList = document.querySelector("#todo-list")
+const addBtn = document.querySelector("#add-btn")
+
+let todos = []
+
+// 미리 저장된 todos가 있는지 확인
+const savedTodos = JSON.parse(localStorage.getItem("todos"));
+
+if (savedTodos) {
+    todos = savedTodos
+    renderTodos()
+}
+
+
+function deleteTodo(index) {
+    todos.splice(index, 1)
+    renderTodos()
+    savedTodos()
+}
+
+// 최신 Todo 데이터를 로컬 스토리지에 저장
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+
+// 최신 Todo 데이터를 화면에 렌더링
+function renderTodos() {
+    todoList.innerHTML = ""
+
+    for (const [index, todo] of todos.entries()) {
+    const li = document.createElement("li")
+    li.className = "list-group-item d-flex justify-content-between align-items-center"
+    li.textContent = todo
+
+    const btn = document.createElement("button")
+    btn.className = "btn btn-sm btn-danger"
+    btn.textContent = "삭제"
+
+    btn.addEventListener("click", () => deleteTodo(index))
+
+    li.appendChild(btn)
+    todoList.appendChild(li)
+    }
+}
+
+function addTodo() {
+    const todo = todoInput.value.trim()
+    todoInput.value = ""
+    
+    if (todo === "") {
+        alert("할 일을 입력하세요")
+        return
+    }
+
+    todos.push(todo)
+    renderTodos()
+
+    // 로컬 스토리지에 저장
+    savedTodos()
+}
+
+// 할 일 추가
+addBtn.addEventListener("click", addTodo)
+
+todoInput.addEventListener("keydown", e => {
+    if (e.isComposing) {
+        return
+    }
+    
+    if (e.key === "Enter") {
+        addTodo()
+    }
+})
